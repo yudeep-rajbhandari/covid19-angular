6f574a53359d25ae9475e652836b41cd8217bf3d
Rajbhandari, Yudeep <i28251@verisk.com>
2020-04-05 19:57:31 +0545

    https change to nginx

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  serverBaseUrl: 'https://thiscovid-backend.herokuapp.com/',
-  serverUrl: 'http://covidnepal.eastus.cloudapp.azure.com:8080/'
+  serverBaseUrl: 'https://thiscovidnepal.com/api/',
+  serverUrl: 'https://thiscovidnepal.com/api/'
 };
