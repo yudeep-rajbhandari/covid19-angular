b76c19637fb4bd92a6ba0e8967624b52dd271259
Rajbhandari, Yudeep <i28251@verisk.com>
2020-04-08 17:28:12 +0545

    AQI added for current time plus scrollable table

--- src/app/hospital-list/hospital-list.component.ts
+++ src/app/hospital-list/hospital-list.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-// import {DataService} from '../core/service/data.service';
+import {Component, OnInit, PipeTransform, ViewChild} from '@angular/core';
 import hospitals = Authentication.hospitals;
 import {HttpBackend, HttpClient} from '@angular/common/http';
 import {NotificationService} from '../core/notification/notification.service';
 import DataPayload = Authentication.DataPayload;
+import {Observable} from 'rxjs';
+import {FormControl} from '@angular/forms';
+import * as jQuery from 'jquery';
+import 'bootstrap-notify';
+const $: any = jQuery;
 
 @Component({
   selector: 'app-hospital-list',
@@ -15,18 +19,29 @@ export class HospitalListComponent implements OnInit {
   databool: boolean;
   Hospitals: hospitals[];
   data: DataPayload;
+  filter = new FormControl('');
+search(text: string, pipe: PipeTransform): hospitals[] {
+  return this.Hospitals.filter(hos => {
+    const term = text.toLocaleLowerCase();
+    return hos.name.toLocaleLowerCase().includes(term)
+    || pipe.transform(hos.district_name.toLocaleLowerCase()).includes(term)
+    || pipe.transform(hos.municipality_name.toLocaleLowerCase()).includes(term);
+  });
+}
   constructor(handler: HttpBackend, private ns: NotificationService, private httpClient: HttpClient) {
     this.httpClient = new HttpClient(handler);
     this.httpClient.get<DataPayload>('https://covidapi.mohp.gov.np/api/v1/health-facility2/').subscribe(data => {
       console.log(data);
       this.data = data;
       this.Hospitals = this.data.results;
+      // this.data.results = this.filter.valueChanges.pipe();
       console.log(this.Hospitals);
       this.databool = true;
     });
   }
 
   ngOnInit() {
+
   }
 
 }
